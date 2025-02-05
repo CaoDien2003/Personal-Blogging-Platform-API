f16ebb5bbfe4cf01a741d4f36267f0ff4584db3f
Nguyen Cao Dien <118111366+CaoDien2003@users.noreply.github.com>
2025-02-05 13:07:39 +0700

    Delete app/config.py

--- app/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'your_secret_key')
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL', 'postgresql://postgres:caodien1403@localhost:5432/blog_db'
-    )
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
